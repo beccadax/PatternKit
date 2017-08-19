bebccfa13849741428efc18a5ad07ba6f3fca383
Brent Royal-Gordon <brent@architechies.com>
2017-08-19 02:38:59 -0700

    Initial Commit

---
+++ PatternKit/PatternKit.h
@@ -0,0 +1,19 @@
+//
+//  PatternKit.h
+//  PatternKit
+//
+//  Created by Brent Royal-Gordon on 8/19/17.
+//  Copyright © 2017 Architechies. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PatternKit.
+FOUNDATION_EXPORT double PatternKitVersionNumber;
+
+//! Project version string for PatternKit.
+FOUNDATION_EXPORT const unsigned char PatternKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PatternKit/PublicHeader.h>
+
+
